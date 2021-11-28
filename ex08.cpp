8fdf1ee5b11349dee4fa542ca411327856332327
Merrek DeBolt <itsugamer@gmail.com>
2021-11-28 14:42:02 -0500

    test

---
+++ ex08.cpp
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Merrek DeBolt
+ */
+
+#include <iostream>
+#include "std_lib_facilities.h"
+
+int main() 
+{
+    int x;
+    cout << "Please enter a value: ";
+    cin >> x;
+
+    cout << "The value " << x << " is an ";
+    if (x % 2 == 0)
+        cout << "even number.";
+    else
+        cout << "odd number.";
+
+    return 0;
+}
